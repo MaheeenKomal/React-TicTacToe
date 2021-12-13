08e9c6628a43a184e8179aa3458ba217174e54af
Maheen Komal <maheenkomal992@gmail.com>
2021-12-13 21:04:04 +0500

    pushed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+function Square(props) {
+  return <h3 onClick={props.onClick}>{props.value}</h3>;
+}
+// function isBoardFull(square) {
+//   for (let i = 0; i < square.length; i++) {
+//     if (square[i] == null) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+function Board() {
+  const [state, setState] = useState(Array(9).fill(null));
+  const [next, setNext] = useState(true);
+
+  const handleClick = (index) => {
+    const square = [...state];
+    if (square[index]) return;
+    square[index] = next ? "X" : "0";
+    setState(square);
+    setNext(!next);
+  };
+  function Winner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+  function isBoardFull(squares) {
+    for (let i = 0; i < squares.length; i++) {
+      if (squares[i] == null) {
+        return false;
+      }
+    }
+    return true;
+  }
+  const renderSquare = (index) => {
+    return <Square value={state[index]} onClick={(e) => handleClick(index)} />;
+  };
+
+  let status;
+  const win = Winner(state);
+  if (state === !null) {
+    status = win
+      ? ` Winner is ${win}`
+      : `Now : ${next ? "Player X" : "Player 0"}`;
+  } else if (state === null) {
+    status = `Draw`;
+  }
+
+  // const box = values.map((numbers) => <h3>{renderSquare(0)}</h3>);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div class="container text-center">
+        <h1>TIC TAC TOE</h1>
+        <h2>{status}</h2>
+        <div className=" board">
+          <div className="d-flex">
+            {renderSquare(0)}
+            {renderSquare(1)}
+            {renderSquare(2)}
+          </div>
+          <div className="d-flex">
+            {renderSquare(3)}
+            {renderSquare(4)}
+            {renderSquare(5)}
+          </div>
+          <div className="d-flex">
+            {renderSquare(6)}
+            {renderSquare(7)}
+            {renderSquare(8)}
+          </div>
+        </div>
+      </div>
+    </>
   );
+}
+function App() {
+  return <Board />;
 }
 
 export default App;
